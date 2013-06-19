77648fa147fd9d1bf622abcc5e1360577dc2794b
Andy Reilly <areilly@kentfirerfa.org>
2013-06-19 10:34:35 -0700

    changed sorting styling, but still missing indicators

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,11 @@
 $(document).on("focus", "[data-behaviour~='datepicker']", function(e){
     $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 0, "autoclose": true })
 });
+
+$(document).ready(function(){
+
+    $('.multiSort').click(function(){
+        window.location.href = $(this).data('link');
+    });
+
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    css_class = column == sort_column ? "#{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to "#{title} <i class='#{direction == "desc" ? "icon-chevron-up" : "icon-chevron-down"}'></i>".html_safe, {:sort => column, :direction => direction}, {:class => css_class}
+
+    if css_class == "asc"
+      header_class = "headerSortDown"
+    elsif css_class == "desc"
+      header_class = "headerSortUp"
+    end
+
+    content_tag("th", title, :"data-link" => url_for(params.merge(:sort => column, :direction => direction, :page => nil)), :class => "header multiSort #{header_class}")
+
   end
 end
