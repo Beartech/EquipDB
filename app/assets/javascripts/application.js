ed9700d8a623d3c2cf740f898d9fdf23b9556cb3
Andy Reilly <areilly@kentfirerfa.org>
2013-07-25 19:31:13 -0700

    got alert messages to fade

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,3 +37,9 @@ $(function () {
         return false;
     });
 })
+
+$(document).ready(function() {
+    $('.alert').delay(500).fadeIn('normal', function() {
+        $(this).delay(2500).fadeOut();
+    });
+});
