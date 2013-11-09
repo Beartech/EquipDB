7909d5fa4c357400d01b5507ec4c8047b26f8f90
Andy Reilly <areilly@kentfirerfa.org>
2013-11-08 18:53:35 -0800

    minor UI tweaks and color mods

--- app/helpers/parts_helper.rb
+++ app/helpers/parts_helper.rb
@@ -1,2 +1,24 @@
 module PartsHelper
+  def inventory_indicator(part)
+
+      if part.inventory > part.minimum
+        raw('<span class="label label-large label-success">Good</span>')
+      elsif part.inventory == part.minimum
+        raw('<span class="label label_large label-warn">Low</span>')
+      elsif part.inventory < part.minimum
+        raw('<span class="label label-large label-important">Warning</span>')
+      end
+
+  end
+
+  def inventory_badge(part)
+    if part.inventory > part.minimum
+      'badge-success'
+    elsif part.inventory == part.minimum
+      'badge-warn'
+    elsif part.inventory < part.minimum
+      'badge-important'
+    end
+  end
+
 end
