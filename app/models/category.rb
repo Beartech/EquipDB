f18d02c414bea2e963c0a26d7f2ef59f3372d538
Andy Reilly <areilly@kentfirerfa.org>
2014-01-14 14:01:14 -0800

    quick switch back to master to update heroku

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,13 @@ class Category < ActiveRecord::Base
   has_many :tools
   validates :name, :presence => true
   validates :name, :uniqueness => true
+
+  def update_cat(dropdown_params)
+    if dropdown_params[:delete_me] == '1'
+      @cat = Category.find_by_name(dropdown_params[:add_category].titleize)
+      @cat.destroy
+    else
+      @cat = Category.new(name: dropdown_params[:add_category].titleize) unless dropdown_params[:add_category] == ''
+    end
+  end
 end
