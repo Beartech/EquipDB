61ebec6d134bfcbc8688075349d17ab6d7f1fc8c
Andy Reilly <areilly@kentfirerfa.org>
2014-02-13 08:36:01 -0800

    change condition to string, update condition

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213080853) do
+ActiveRecord::Schema.define(version: 20140213161804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,7 +122,7 @@ ActiveRecord::Schema.define(version: 20140213080853) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "note"
-    t.integer  "condition"
+    t.string   "condition"
     t.text     "old_location"
     t.string   "model"
     t.boolean  "loaner",         default: false
