51f4f0afc158f768fd87eae3ae88d16f9442d56b
Andy Reilly <areilly@kentfirerfa.org>
2014-03-03 07:40:05 -0800

    changed ann. serv. indicators to helper methods

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -12,4 +12,22 @@ module ToolsHelper
       Tool.where(category_id: tabnum).count
   end
 
+  def annual_service?(tool)
+    tool.services.each do |service|
+      if service.completed
+        if (service.service_type.name =~ /annual/i) && (service.completed.year == DateTime.now.year)
+          return true
+        end
+      end
+    end
+    false
+  end
+
+  def app_annual_service?(app)
+    app.tools.each do |tool|
+      return false unless annual_service?(tool)
+    end
+    true
+  end
+
 end
