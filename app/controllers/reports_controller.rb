2369abbe9549c85c436868e41c809e2264b7cd68
Andy Reilly <areilly@kentfirerfa.org>
2015-01-15 16:29:59 -0800

    setting up for reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,53 @@
 class ReportsController < ApplicationController
+  before_action :set_tool, only: [:show, :edit, :update, :destroy, :toggle_in_service, :tool_report ]
   before_action :role_required
 
+  # GET /reports
+  # GET /reports.json
   def index
+    @reports = Report.all
 
   end
 
+  # GET /reports/1
+  # GET /reports/1.json
+  def show
+  end
+
+  # GET /reports/new
+  def new
+    @report = Report.new
+
+  end
+
+  # GET /report/1/edit
+  def edit
+  end
+
+  # POST /reports
+  # POST /reports.json
+  def create
+
+    respond_to do |format|
+
+    end
+  end
+
+  # PATCH/PUT /reoprts/1
+  # PATCH/PUT /reports/1.json
+  def update
+    respond_to do |format|
+
+    end
+  end
+
+  # DELETE /reports/1
+  # DELETE /reports/1.json
+  def destroy
+    respond_to do |format|
+
+    end
+  end
 
 
   private
