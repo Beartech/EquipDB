e04047f18c9243bd911c2fc3a87839e82d8f7439
Andy Reilly <areilly@kentfirerfa.org>
2015-03-03 12:15:07 -0800

    added part of parts_table sql that was missing

--- db/dashboard_view.sql
+++ db/dashboard_view.sql
@@ -9,7 +9,6 @@ SELECT dashviews.*
   ORDER BY dashviews.loc_name;
 
 
-
 SELECT ann_serv_complete.*
   FROM (
     SELECT tools.id, tools.name, services.id serv_id, service_types.name serv_type, services.completed comp
@@ -17,13 +16,14 @@ SELECT ann_serv_complete.*
     INNER JOIN tools ON tools.id = services.tool_id
     INNER JOIN service_types ON service_types.id = services.service_type_id)
   AS ann_serv_complete
-WHERE (serv_type LIKE '%Annual%' OR serv_type LIKE '%New%')  AND  date_part('year', comp) = date_part('year', now())
-  ;
+WHERE (serv_type LIKE '%Annual%' OR serv_type LIKE '%New%')  AND  date_part('year', comp) = date_part('year', now());
+
 
 SELECT parts.*, alias_skus
   FROM parts
-    JOIN (
+    LEFT JOIN (
            SELECT part_id, array_agg(sku) alias_skus
            FROM part_aliases GROUP BY part_id)
       AS sub1
       ON parts.id = sub1.part_id;
+
